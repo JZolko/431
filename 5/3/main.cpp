6f2670e5b8f97bd0c2a5dc41edebb03bdd2f8308
Jake Zolkosky <zolkosky@cse.msu.edu>
2022-11-11 16:59:38 -0500

    5 maybe

--- 5/3/main.cpp
+++ 5/3/main.cpp
@@ -1,23 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <list>
+#include <vector>
 #include <iostream>
-#include <sstream>
 
 
 
 int main(){
-	int N = scanf("%d", &N);
-	std::string line;
-	for(int i = 0; i < N; i++){
-		std::getline(std::cin, line);
-		std::istringstream iss(line);
-		std::string word;
-		while(iss >> word){
-			std::cout << word << std::endl;
-		}
-	}
+
+	// get an int input from the user
+	int number_of_test_cases;
+    int brick_types, target_length;
+
+	std::cin >> number_of_test_cases;
+
+
+    for(int first = 0; first < number_of_test_cases; first++){
+        // get an int pair from user
+        std::cin >> brick_types >> target_length;
+        // make an int vector of size N
+        std::vector<int> bricks;
+        for(int second = 0; second < brick_types; second++){
+            // add an int to the vector
+            int temp;
+            std::cin >> temp;
+            bricks.push_back(temp);
+        }
+
+        // make a vector of 0s with size L + 1
+        std::vector<int> table;
+        for(int zeros = 0; zeros < target_length + 1; zeros++)
+            table.push_back(0);
+
+        table[0] = 1;
+
+        for(int i = 0; i < brick_types; i++)
+            for(int j = table[i]; j <= target_length ; j--)
+                table[j] = table[j] + table[j - bricks[i]];
+
+        if(table[-1])
+            std::cout << table[-1] % 1000000009 << std::endl;
+
+
+
+
+    }
+
+
+
+
 	return 0;
 }
